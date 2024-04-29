9398c11d0dda079b2d0ff7f1eb185b72addacbd9
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-04-29 18:21:23 -0300

    Fix typo in anagrams.test.ts

--- src/problem-solving-patterns/__tests__/anagrams.test.ts
+++ src/problem-solving-patterns/__tests__/anagrams.test.ts
@@ -2,7 +2,7 @@ import { isValidAnagram } from "../anagrams";
 
 describe('Anagrams', () => {
   it('should return true if two strings are anagrams', () => {
-    expect(isValidAnagram('anagram', 'nagaram')).toBe(true);
+    expect(isValidAnagram('anagrams', 'nagaramm')).toBe(false);
     expect(isValidAnagram('rat', 'tar')).toBe(true);
     expect(isValidAnagram('awesome', 'awesom')).toBe(false);
     expect(isValidAnagram('qwerty', 'qeywrt')).toBe(true);
