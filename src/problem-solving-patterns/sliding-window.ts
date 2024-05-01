4d7fc7c3996e1a9b3dca9899bf33ae690bfdbd7d
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-01 19:25:57 -0300

    Add sliding-window.ts file with maxSubarraySum function

---
+++ src/problem-solving-patterns/sliding-window.ts
@@ -0,0 +1,19 @@
+export function maxSubarraySum(arr: number[], num: number): number | null{
+  if (arr.length < num) return null;
+
+  let maxSum = 0;
+  let tempSum = 0;
+
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
