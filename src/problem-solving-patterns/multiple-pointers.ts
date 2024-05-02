36184b443e2a4dcfc1d5e68b50487c4085944090
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-01 22:29:44 -0300

    Add isSubsequence function to multiple-pointers.ts

--- src/problem-solving-patterns/multiple-pointers.ts
+++ src/problem-solving-patterns/multiple-pointers.ts
@@ -62,3 +62,26 @@ export function averagePair(arr: number[], target: number): boolean {
 
   return false;
 }
+
+/**
+ * Multiple Pointers - isSubsequence
+ * Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+ */
+export function isSubsequence(str1: string, str2: string): boolean {
+  let i = 0;
+  let j = 0;
+
+  while (j < str2.length) {
+    if (str1[i] === str2[j]) {
+      i++;
+    }
+
+    if (i === str1.length) {
+      return true;
+    }
+
+    j++;
+  }
+
+  return false;
+}
