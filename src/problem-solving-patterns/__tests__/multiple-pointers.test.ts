f894d867b3304f29a1c2bc2c8cd9cb4f57898fbb
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-01 22:10:38 -0300

    Add averagePair function unit tests to multiple-pointers.test.ts

--- src/problem-solving-patterns/__tests__/multiple-pointers.test.ts
+++ src/problem-solving-patterns/__tests__/multiple-pointers.test.ts
@@ -1,4 +1,4 @@
-import { countUniqueValues, sumZero } from "../multiple-pointers";
+import { averagePair, countUniqueValues, sumZero } from "../multiple-pointers";
 
 describe('Multiple Pointers - sumZero', () => {
   it('should return the first pair of numbers that sum to zero', () => {
@@ -13,5 +13,14 @@ describe('Multiple Pointers - countUniqueValues', () => {
     expect(countUniqueValues([1, 1, 1, 1, 1, 2])).toBe(2);
     expect(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])).toBe(7);
     expect(countUniqueValues([])).toBe(0);
+  })
+});
+
+describe('Multiple Pointers - averagePair', () => {
+  it('should return true if there is a pair of values in the array where the average of the pair equals the target average', () => {
+    expect(averagePair([1,2,3],2.5)).toBe(true);
+    expect(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)).toBe(true);
+    expect(averagePair([-1, 0, 3, 4, 5, 6], 4.1)).toBe(false);
+    expect(averagePair([], 4)).toBe(false);
   })
 });
