97a81370e54348b34e12c3f355ca0a1689e406c3
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-01 21:47:20 -0300

    Add unit tests for frequency-counter-pattern functions

--- src/problem-solving-patterns/__tests__/frequency-counter-pattern.test.ts
+++ src/problem-solving-patterns/__tests__/frequency-counter-pattern.test.ts
@@ -1,4 +1,4 @@
-import { sameFrequency } from "../frequency-counter-pattern";
+import { areThereDuplicates, sameFrequency, sameFrequencyOfNumbers } from '../frequency-counter-pattern';
 
 describe("frequency counter pattern", () => {
   it("should return false if the arrays have different lengths", () => {
@@ -11,5 +11,30 @@ describe("frequency counter pattern", () => {
 
   it("should return true if the second array contains the square of the elements of the first array", () => {
     expect(sameFrequency([1, 2, 3, 2], [1, 4, 9, 4])).toBe(true);
+  });
+});
+
+describe("frequency counter pattern - Same Frequency of numbers", () => {
+  it("should return false if the numbers have different lengths", () => {
+    expect(sameFrequencyOfNumbers(123, 12)).toBe(false);
+  });
+
+  it("should return true if the numbers have the same frequency", () => {
+    expect(sameFrequencyOfNumbers(3589578, 5879385)).toBe(true);
+  });
+
+  it("should return true if the second number contains the same frequency of numbers as the first number", () => {
+    expect(sameFrequencyOfNumbers(123, 321)).toBe(true);
+  });
+});
+
+describe("frequency counter pattern - areThereDuplicates", () => {
+  it("should return false if there are no duplicates", () => {
+    expect(areThereDuplicates(1, 2, 3)).toBe(false);
+  });
+
+  it("should return true if there are duplicates", () => {
+    expect(areThereDuplicates(1, 2, 2)).toBe(true);
+    expect(areThereDuplicates('a', 'b', 'c', 'a')).toBe(true);
   });
 });
